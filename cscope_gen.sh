3fff9d0f73d3d1ac0a79e08ae2cfb84ae17f89bf
Max Z. Chao <max.z.chao@gmail.com>
2011-10-31 10:11:00 +1100

    Update cscope_gen.sh: support "*.inc"

--- cscope_gen.sh
+++ cscope_gen.sh
@@ -4,6 +4,7 @@ find . -name "*.[chly]" \
 -o -name "*.cpp"        \
 -o -name "*.cxx"        \
 -o -name "*.cc"         \
+-o -name "*.inc"        \
 -o -name "*.hpp"        \
 -o -name "*.hh"         \
 -o -name "*.py"         \
